609df16637832004cd47f31e38fabfe5a29caa8e
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-11-07 12:38:49 +0100

    Photo done and Stats in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Photo from "@/components/Home/Photo";
 import Socials from "@/components/Home/Socials";
+import Stats from "@/components/Home/Stats";
 import { Button } from "@/components/ui/button";
 import { FileDown } from "lucide-react";
 
@@ -9,7 +10,7 @@ export default function Home() {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* Text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Engineer</span>
             <h1 className="h1 mb-6">
               Hello, I&apos;m <br />{" "}
@@ -41,11 +42,13 @@ export default function Home() {
           </div>
 
           {/* Photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      
+      <Stats />
     </section>
   );
 }
