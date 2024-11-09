75b9d5c025f4549b1583493311fb023699a014c7
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-11-09 09:19:55 +0100

    Deployment error fixed

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,37 +1,37 @@
 "use client";
 
-import { BsArrowDownRight } from "react-icons/bs";
+// import { BsArrowDownRight } from "react-icons/bs";
 
-const services = [
-  {
-    num: "01",
-    title: "Web Development",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
-    href: "",
-  },
-  {
-    num: "02",
-    title: "Web Development",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
-    href: "",
-  },
-  {
-    num: "03",
-    title: "Web Development",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
-    href: "",
-  },
-  {
-    num: "04",
-    title: "SEO",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
-    href: "",
-  },
-];
+// const services = [
+//   {
+//     num: "01",
+//     title: "Web Development",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
+//     href: "",
+//   },
+//   {
+//     num: "02",
+//     title: "Web Development",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
+//     href: "",
+//   },
+//   {
+//     num: "03",
+//     title: "Web Development",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
+//     href: "",
+//   },
+//   {
+//     num: "04",
+//     title: "SEO",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia delectus quibusdam, quis amet voluptate minus culpa ad molestiae perspiciatis perferendis numquam eveniet ab laboriosam nesciunt dolorum voluptatum provident. Autem!",
+//     href: "",
+//   },
+// ];
 
 const Services = () => {
   return <div>Services</div>;
